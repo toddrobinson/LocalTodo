31c3d5e0e31bc446a32b4443b9024367cecd5205
Todd Robinson <toddsinclairrobinson@gmail.com>
2015-10-01 17:03:54 -0400

    Add controller, app and styles. Add functions for todo list.

---
+++ js/controllers/MainController.js
@@ -0,0 +1,32 @@
+app.controller('MainController', ['$scope', function($scope){
+  $scope.title = "Local Todo.";
+  if (!localStorage.getItem('todos')) {
+        $scope.todos = [];
+        $scope.todos.push("Example todo!");
+      }
+  else {
+      $scope.todos = localStorage.getItem('todos').split(',');
+  }
+  $scope.addTodo = function() {
+    var taskInput = $('input').filter("#taskEntry");
+    var task = $(taskInput).val();
+    $(taskInput).val("");
+
+    $scope.todos.push(task);
+    $scope.saveList();
+    //alert(Object.keys($scope.todos).length);
+  };
+
+  $scope.deleteTodo = function(todo) {
+      var pos = $scope.todos.indexOf(todo);
+      $scope.todos.splice(pos,1);
+      $scope.todos.forEach(function (e,i,a) {console.log(e);});
+      $scope.saveList();
+  };
+
+  $scope.saveList = function() {
+      var todoString = $scope.todos.toString();
+      console.log(todoString);
+      localStorage.setItem("todos", todoString);
+  };
+}]);
