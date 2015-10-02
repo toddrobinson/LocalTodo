d08e84ef35151fe4d82b5a3688d14b7f78fd5019
Todd Robinson <toddsinclairrobinson@gmail.com>
2015-10-02 13:30:42 -0400

    Add array of todos into an object. Less convoluted then spliting an array string on the commas. Todos might contain commas, need to support that.
    Signed-off-by: Todd Robinson <toddsinclairrobinson@gmail.com>

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,31 +1,32 @@
 app.controller('MainController', ['$scope', function($scope){
   $scope.title = "Local To-do.";
   if (!localStorage.getItem('todos')) {
-        $scope.todos = [];
-        $scope.todos.push("Example todo!");
+        $scope.todos = {};
+        $scope.todos.list = [];
+        $scope.todos.list.push("Example todo!");
       }
   else {
-      $scope.todos = localStorage.getItem('todos').split(',');
+      $scope.todos = JSON.parse(localStorage.getItem('todos'));
   }
   $scope.addTodo = function() {
     var taskInput = $('input').filter("#taskEntry");
     var task = $(taskInput).val();
     $(taskInput).val("");
 
-    $scope.todos.push(task);
+    $scope.todos.list.push(task);
     $scope.saveList();
     //alert(Object.keys($scope.todos).length);
   };
 
   $scope.deleteTodo = function(todo) {
-      var pos = $scope.todos.indexOf(todo);
-      $scope.todos.splice(pos,1);
-      $scope.todos.forEach(function (e,i,a) {console.log(e);});
+      var pos = $scope.todos.list.indexOf(todo);
+      $scope.todos.list.splice(pos,1);
+      $scope.todos.list.forEach(function (e,i,a) {console.log(e);});
       $scope.saveList();
   };
 
   $scope.saveList = function() {
-      var todoString = $scope.todos.toString();
+      var todoString = JSON.stringify($scope.todos);
       console.log(todoString);
       localStorage.setItem("todos", todoString);
   };
