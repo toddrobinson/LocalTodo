4115dea8f71bea50c79445e4a9cbf6286405a4c4
Todd Robinson <toddsinclairrobinson@gmail.com>
2015-10-02 13:39:03 -0400

    Add foundation css. Remove debug statements from main controller.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -12,22 +12,18 @@ app.controller('MainController', ['$scope', function($scope){
     var taskInput = $('input').filter("#taskEntry");
     var task = $(taskInput).val();
     $(taskInput).val("");
-
     $scope.todos.list.push(task);
     $scope.saveList();
-    //alert(Object.keys($scope.todos).length);
   };
 
   $scope.deleteTodo = function(todo) {
       var pos = $scope.todos.list.indexOf(todo);
       $scope.todos.list.splice(pos,1);
-      $scope.todos.list.forEach(function (e,i,a) {console.log(e);});
       $scope.saveList();
   };
 
   $scope.saveList = function() {
       var todoString = JSON.stringify($scope.todos);
-      console.log(todoString);
       localStorage.setItem("todos", todoString);
   };
 }]);
