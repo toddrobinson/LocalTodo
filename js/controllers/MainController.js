1673828cfd1ccd309d54fcc32d8c404ab0bea681
Todd Robinson <toddsinclairrobinson@gmail.com>
2015-10-05 12:48:43 -0400

    Added comments to maincontroller methods

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,17 +1,36 @@
+/**
+ * [MainController for todo application]
+ * @param  {string} 'MainController'  [Name of controller]
+ * @param  {} ['$scope'         [Scope dependency]
+ * @return {}
+ */
 app.controller('MainController', ['$scope', function($scope){
   $scope.title = "Local To-do";
+  /**
+   * Setting the todo object if it exists in localStorage
+   * Otherwise, putting an example to-do.
+   * If there are no to-dos, but item exists, no example to-do, becauase it
+   * isn't the first visit.
+   * $scope.todo object that contains an array $scope.todo.list that contains
+   * the to-dos
+   */
   if (!localStorage.getItem('todos')) {
         $scope.todos = {};
         $scope.todos.list = [];
-        $scope.todos.list.push("Example todo!");
+        $scope.todos.list.push("Example to-do!");
       }
   else {
       $scope.todos = JSON.parse(localStorage.getItem('todos'));
   }
+  /**
+   * [Takes to-do from text field, adds it to scope variable, calls save list function]
+   * @return {} []
+   */
   $scope.addTodo = function() {
     var taskInput = $('input').filter("#taskEntry");
     var task = $(taskInput).val();
     $(taskInput).val("");
+    //Todo can't be blank, and can't already be in array
     if (task && $scope.todos.list.indexOf(task) < 0) {
       $scope.todos.list.push(task);
       $scope.saveList();
@@ -22,12 +41,20 @@ app.controller('MainController', ['$scope', function($scope){
 
   };
 
+  /**
+   * [Deletes todo from scope variable, calls save list function]
+   * @param  {todo in array} todo
+   */
   $scope.deleteTodo = function(todo) {
       var pos = $scope.todos.list.indexOf(todo);
       $scope.todos.list.splice(pos,1);
       $scope.saveList();
   };
 
+  /**
+   * Save the to-do list from scope variable.
+   * Saves into localStorage as JSON
+   */
   $scope.saveList = function() {
       var todoString = JSON.stringify($scope.todos);
       localStorage.setItem("todos", todoString);
